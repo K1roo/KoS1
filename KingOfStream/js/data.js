ee858d978800baaaec1a11c7c4950690a4f635de
K1roo <96072937+K1roo@users.noreply.github.com>
2022-03-30 11:52:29 +0200

    commit on main - new app KOS

--- KingOfStream/js/data.js
+++ KingOfStream/js/data.js
@@ -1,37 +1,72 @@
-var jsonData = [
+// creating an array and passing the number, questions, options, and answers aka our JSON file
+let questions = [
     {
-        "q" : "The common element which describe the web page, is ?",
-        "opt1" : "heading",
-        "opt2" : "paragraph",
-        "opt3" : "All of these",
-        "answer" : "All of these"
-    },
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
     {
-        "q" : "HTML stands for?",
-        "opt1" : "Hyper Text Markup Language",
-        "opt2" : "High Text Markup Language",
-        "opt3" : "Hyper Tabular Markup Language",
-        "answer" : "Hyper Text Markup Language"
-    },
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
     {
-        "q" : "which of the following tag is used to mark a begining of paragraph ?",
-        "opt1" : "TD",
-        "opt2" : "br",
-        "opt3" : "P",
-        "answer" : "P"
-    },
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
     {
-        "q" : "From which tag descriptive list starts ?",
-        "opt1" : "LL",
-        "opt2" : "DL",
-        "opt3" : "DD",
-        "answer" : "DL"
-    },
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
     {
-        "q" : "Correct HTML tag for the largest heading is _____",
-        "opt1" : "h1",
-        "opt2" : "h6",
-        "opt3" : "heading",
-        "answer" : "h1"
-    }
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
 ];
